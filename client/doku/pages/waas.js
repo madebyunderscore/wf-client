ea9b826334a674643729eac2474ea9d9ae3b2530
madebyunderscore <accounts@madebyunderscore.com>
2025-04-16 10:04:10 +0800

    Updated waas to not have additional file

--- client/doku/pages/waas.js
+++ client/doku/pages/waas.js
@@ -1,38 +1,44 @@
-import KeenSlider from 'keen-slider';
+import { intializekeenSlider } from 'keen-slider';
 
-document.addEventListener("DOMContentLoaded", function () {
-    const slider = document.querySelector("#waas-reviews-slider");
-    if (!slider) return;
-
-    const animation = {
-      duration: 100000,
-      easing: t => t,
-    };
-
-    new KeenSlider(slider, {
-      loop: true,
-      renderMode: 'performance',
-      drag: true,
-      slides: {
-        perView: 3,
-        spacing: 24,
-      },
-      breakpoints: {
-        '(max-width: 991px)': {
-          slides: { perView: 2, spacing: 24 },
-        },
-        '(max-width: 767px)': {
-          slides: { perView: 1, spacing: 24 },
+const animation = {
+    duration: 100000,
+    easing: function (t) {
+      return t;
+    },
+  };
+  
+  const careerSliderOptions = {
+    loop: true,
+    renderMode: 'performance',
+    drag: true,
+    slides: {
+      perView: 3,
+      spacing: 24,
+    },
+    breakpoints: {
+      '(max-width: 991px)': {
+        slides: {
+          perView: 2,
+          spacing: 24,
         },
       },
-      created(s) {
-        s.moveToIdx(5, true, animation);
+      '(max-width: 767px)': {
+        slides: {
+          perView: 1,
+          spacing: 24,
+        },
       },
-      updated(s) {
-        s.moveToIdx(s.track.details.abs + 5, true, animation);
-      },
-      animationEnded(s) {
-        s.moveToIdx(s.track.details.abs + 5, true, animation);
-      },
-    });
-  });
+    },
+    created: function (slider) {
+      slider.moveToIdx(5, true, animation);
+    },
+    updated: function (slider) {
+      slider.moveToIdx(slider.track.details.abs + 5, true, animation);
+    },
+    animationEnded: function (slider) {
+      slider.moveToIdx(slider.track.details.abs + 5, true, animation);
+    },
+  };
+  
+  // Initialize the career slider
+  initializeKeenSlider('#waas-reviews-slider', careerSliderOptions);
